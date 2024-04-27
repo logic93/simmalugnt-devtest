74e5852c6a0150379f2b373134026f9de1085e20
logic93 <jeanmelki1@gmail.com>
2024-04-28 00:17:35 +0200

    Add newsletter signup form and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className="page-wrapper">
+      <div className="signup-wrapper">
+        <div className="text-wrapper">
+          <h1>Sign up to our newsletter</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consecte adipiscing elit praesent
+            sodales purus magna, eget lacinia sapien hendrerit.
+          </p>
+        </div>
+        <form action="/">
+          <label className="hidden" htmlFor="email" />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            placeholder="Email"
+          />
+          <button type="submit">sign up</button>
+        </form>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,8 +8,39 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      spacing: {
+        "4.5rem": "4.5rem",
+      },
+      borderRadius: {
+        "2.5rem": "2.5rem",
+      },
       fontFamily: {
         sans: ["SimmaLugnt-Sans", "var(--font-simmalugnt)"],
+      },
+      fontSize: {
+        base: [
+          "1rem",
+          {
+            lineHeight: "1.25rem",
+            fontWeight: "700",
+          },
+        ],
+        heading: [
+          "2.5rem",
+          {
+            lineHeight: "3rem",
+            letterSpacing: "-0.03rem",
+            fontWeight: "700",
+          },
+        ],
+        "2xl": [
+          "1.5rem",
+          {
+            lineHeight: "2rem",
+            letterSpacing: "-0.03rem",
+            fontWeight: "700",
+          },
+        ],
       },
     },
   },
