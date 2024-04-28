cb2eb0bb02e83611e4c73eb901edba6e3fcc83f5
logic93 <jeanmelki1@gmail.com>
2024-04-28 18:50:54 +0200

    Refactor page to use components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,20 @@
+"use client";
+
+import { NewsletterCard } from "@/components/NewsletterCard";
+
 export default function Home() {
+  async function formAction() {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log("test");
+  }
+
   return (
     <div className="page-wrapper">
-      <div className="signup-wrapper">
-        <div className="text-wrapper">
-          <h1>Sign up to our newsletter</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consecte adipiscing elit praesent
-            sodales purus magna, eget lacinia sapien hendrerit.
-          </p>
-        </div>
-        <form action="/">
-          <label className="hidden" htmlFor="email" />
-          <input
-            type="email"
-            id="email"
-            name="email"
-            required
-            placeholder="Email"
-          />
-          <button type="submit">sign up</button>
-        </form>
-      </div>
+      <NewsletterCard
+        action={formAction}
+        pendingTitle="signing up..."
+        buttonTitle="sign up"
+      />
     </div>
   );
 }
