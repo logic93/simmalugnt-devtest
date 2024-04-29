f59fa0022527afa2be8e45a530a632633ca9d01f
logic93 <jeanmelki1@gmail.com>
2024-04-29 23:08:11 +0200

    Refactor server action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,23 +1,25 @@
 "use server";
 
+import { variables } from "@/config";
+import { States } from "@/config/enums";
+import { revalidatePath } from "next/cache";
 import z from "zod";
 
-const schema = z.object({
-  email: z.string().email(),
-});
+const emailSchema = z.string().email();
 
-export async function formAction(formData: FormData) {
-  const results = schema.safeParse({
-    email: formData.get("email"),
-  });
+export async function onSignupNewsletter(prevState: any, formData: FormData) {
+  const email = formData.get("email");
+  const results = emailSchema.safeParse(email);
 
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  if (results.success) {
-    console.log("valid");
-  }
+  await new Promise((resolve) => setTimeout(resolve, variables.SERVER_DELAY));
 
   if (!results.success) {
-    throw new Error("error");
+    return States.ERROR;
   }
+
+  if (results.success) {
+    return States.SUCCESS;
+  }
+
+  revalidatePath("/");
 }
